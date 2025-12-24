8759e4461701b2e9555116dc74138c534a70a194
alzahraa-mahmoud <alzahraa.mahmoud@vodafone.com>
2025-12-24 15:00:52 +0200

    edit ci file v4

--- ci/uploadAPK.sh
+++ ci/uploadAPK.sh
@@ -1,12 +1,7 @@
 echo "Instalando APK no emulador 5554..."
 adb -s emulator-5554 install -t -g ./src/test/resources/Utils/General-Store.apk
 
-echo "Instalando APK no emulador 5556..."
-adb -s emulator-5556 install -t -g ./src/test/resources/Utils/General-Store.apk
 
-# 3. Verificação da instalação
 echo "Verificando pacotes no emulador 5554:"
 adb -s emulator-5554 shell pm list packages | grep calculator || echo "Pacote não encontrado no emulador 5554"
 
-#echo "Verificando pacotes no emulador 5556:"
-adb -s emulator-5556 shell pm list packages | grep calculator || echo "Pacote não encontrado no emulador 5556"
