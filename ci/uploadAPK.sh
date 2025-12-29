d7d8fbdaf86a0ee87119e316dbd914c0c86db15f
zahraamahmoud <zahraamahmoud@users.noreply.github.com>
2025-12-29 14:46:26 +0000

    deploy: bb35673c74b9f1b8bbd94712ab656cb9641a3d97

--- ci/uploadAPK.sh
+++
@@ -1,7 +0,0 @@
-echo "Instalando APK no emulador 5554..."
-adb -s emulator-5554 install -t -g ./src/test/resources/Utils/General-Store.apk
-
-
-echo "Verificando pacotes no emulador 5554:"
-adb -s emulator-5554 shell pm list packages | grep calculator || echo "Pacote não encontrado no emulador 5554"
-
